b557a5d49e506574c23fda8294f957ff255438ad
David Sanders <dsanders@rapilabs.com>
2013-07-30 22:39:18 +1000

    Derp

--- pewbot/commands/pew.py
+++ pewbot/commands/pew.py
@@ -4,7 +4,7 @@ class Command(PewbotCommand):
     help = "Say pew again, I dare you!"
     def handle(self, message):
 				if 'pewbot' not in message:	
-						if 'pew?' in message
+						if 'pew?' in message:
 								return ["That's right I said pew muthafucka!"]
-						if 'pew' in message
+						if 'pew' in message:
 								return ['pew!']
