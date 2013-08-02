0a3399e49e47bf46f31de8269386344b93a2178d
David Sanders <dsanders@rapilabs.com>
2013-08-02 22:31:35 +1000

    Adding pork roll reply

--- pewbot/commands/pew.py
+++ pewbot/commands/pew.py
@@ -3,8 +3,11 @@ from pewbot.commands.base import PewbotCommand
 class Command(PewbotCommand):
     help = "Say pew again, I dare you!"
     def handle(self, message):
-				if 'pewbot' not in message:	
-						if 'pew?' in message:
-								return ["That's right I said pew muthafucka!"]
-						if 'pew' in message:
-								return ['pew!']
+        if 'pewbot' not in message:    
+            if 'pew?' in message:
+                return ["That's right I said pew muthafucka!"]
+            if 'pew' in message:
+                return ['pew!']
+        if 'pork roll' in message:
+            return ['http://s3.amazonaws.com/barkpost-assets/50+GIFs/7.gif', 'Did somebody say pork roll?']
+
