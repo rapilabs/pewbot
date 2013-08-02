c87ff82f4b3110492bae1bcad9b8676441b1a2ae
David Sanders <dsanders@rapilabs.com>
2013-08-02 22:46:23 +1000

    Adding response to coffee

--- pewbot/commands/pew.py
+++ pewbot/commands/pew.py
@@ -10,4 +10,6 @@ class Command(PewbotCommand):
                 return ['pew!']
         if 'pork roll' in message:
             return ['http://s3.amazonaws.com/barkpost-assets/50+GIFs/7.gif', 'Did somebody say pork roll?']
+        if 'coffee' in message:
+            return ['https://lh5.googleusercontent.com/-MlDz2f26eqQ/UY8qz1syY_I/AAAAAAAAA4o/EWq1ENUb71Y/95ff6c96.gif', 'Did somebody say coffee?']
 
